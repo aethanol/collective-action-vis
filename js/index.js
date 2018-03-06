7f132115aab1c6905b94f070a1fdec19997841af
aethanol <aethan@uw.edu>
2018-03-05 17:44:12 -0800

    fuck this its broken

--- js/index.js
+++ js/index.js
@@ -2,28 +2,44 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
-var pro = d3.geoAlbersUsa();
+// var pro = d3.geoAlbersUsa();
 
-var path = d3.geoPath();
+// var path = d3.geoPath();
 
-var x = d3.scaleLinear()
+var projection = d3.geo.albers()
+    .rotate([96, 0])
+    .center([-.6, 38.7])
+    .parallels([29.5, 45.5])
+    .scale(1070)
+    .translate([width / 2, height / 2])
+    .precision(.1);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var x = d3.scale.linear()
     .domain([1, 10])
     .rangeRound([600, 860]);
 
-var color = d3.scaleThreshold()
-    .domain(d3.range(2, 10))
-    .range(d3.schemeBlues[9]);
+// var color = d3.scale.threshold()
+//     .domain(d3.range(2, 10))
+//     .range(d3.scheme.blues[9]);
 
-var size = d3.scaleLinear().range([2, 25]);
+var size = d3.scale.linear().range([2, 25]);
 
-var g = svg.append("g")
-    .call(d3.zoom()
-    .scaleExtent([1 / 2, 4])
-    .on("zoom", zoomed));
+// var g = svg.append("g")
+//     .call(d3.zoom()
+//     .scaleExtent([1 / 2, 4])
+//     .on("zoom", zoomed));
 
-function zoomed() {
-    g.attr("transform", d3.event.transform);
-}
+// function zoomed() {
+//     g.attr("transform", d3.event.transform);
+// }
+
+// Define the div for the tooltip
+var div = d3.select("#vis").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
   
 // var g = svg.append("g")
 //     .attr("class", "key")
@@ -57,45 +73,93 @@ function zoomed() {
 //     .tickValues(color.domain()))
 //   .select(".domain")
 //     .remove();
-d3.queue()
-    .defer(d3.json, "data/us-10m.v1.json")
-    .defer(d3.json, "data/protests.json")
-    .await(ready);
+// d3.queue()
+//     .defer(d3.json, "data/us-10m.v1.json")
+//     .defer(d3.json, "data/protests.json")
+//     .await(ready);
+d3.json("data/us-10m.v1.json", function(error, us) {
 
-function ready(error, us, protests) {
-    if (error) throw error;
-        // points
-    // aa = [-122, 47];
-    // bb = [-122.389809, 37.72728];
-
-    // // states = topojson.feature(us, us.objects.states).features
-    // console.log(pro(aa),pro(bb));
-    size.domain([0, d3.max(protests, function(d) { return d.partict; })]);
+    d3.json("data/protests.json", function(error, protests) {
+        size.domain([0, d3.max(protests, function(d) { return d.partict; })]);
     //console.log(protests);
-    g.append("g")
+
+    // build map path
+    svg.append("g")
         .attr("class", "counties")
         .selectAll("path")
         .data(topojson.feature(us, us.objects.counties).features)
         .enter().append("path")
-        .attr("fill", color(3)) // TODO: figure out how to do this without color func above?
+        .attr("fill", "blue") // TODO: figure out how to do this without color func above?
         .attr("d", path);
     // add states from topojson
-    g.append("path")
+    svg.append("path")
         .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
         .attr("class", "states")
         .attr("d", path);
+    });
+});
+// function ready(error, us, protests) {
+//     if (error) throw error;
+//         // points
+//     // aa = [-122, 47];
+//     // bb = [-122.389809, 37.72728];
 
-    // svg.append("circle")
-    //     .data([aa,bb]).enter()
-    //     .attr("cx")
+//     // // states = topojson.feature(us, us.objects.states).features
+//     // console.log(pro(aa),pro(bb));
+//     size.domain([0, d3.max(protests, function(d) { return d.partict; })]);
+//     //console.log(protests);
 
-  //add a dot
-  g.selectAll("circle")
-		.data(protests).enter()
-        .append("circle")
-        .attr("cx", function (d) { return pro(JSON.parse(d.location))[0]; })
-		.attr("cy", function (d) { return pro(JSON.parse(d.location))[1]; })
-        .attr("r", function (d) { return size(d.partict); })
-        .attr("fill", "red")
-        .on("click", function(d) { console.log(d); });
-}
+//     // build map path
+//     g.append("g")
+//         .attr("class", "counties")
+//         .selectAll("path")
+//         .data(topojson.feature(us, us.objects.counties).features)
+//         .enter().append("path")
+//         .attr("fill", color(3)) // TODO: figure out how to do this without color func above?
+//         .attr("d", path);
+//     // add states from topojson
+//     g.append("path")
+//         .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+//         .attr("class", "states")
+//         .attr("d", path);
+
+//     // svg.append("circle")
+//     //     .data([aa,bb]).enter()
+//     //     .attr("cx")
+//     var defaultFill = "red";
+//   //add a dot
+//   g.selectAll("circle")
+// 		.data(protests).enter()
+//         .append("circle")
+//         .attr("cx", function (d) { return projection(JSON.parse(d.location))[0]; })
+// 		.attr("cy", function (d) { return projection(JSON.parse(d.location))[1]; })
+//         .attr("r", function (d) { return size(d.partict); })
+//         .attr("fill", "red")
+//         .on("mouseover", function(d){
+//             d3.select(this)
+//             .attr("fill", "orange");
+//             console.log(d);
+//             div.transition()		
+//                 .duration(200)		
+//                 .style("opacity", .9);		
+//             div.html("HELLO")	
+//                 .style("left", (d3.event.pageX) + "px")		
+//                 .style("top", (d3.event.pageY - 28) + "px");	
+//             })
+//             // var prop = d.properties;
+      
+//             // var string = "<p><strong>Market Area Name</strong>: " + prop.dma1 + "</p>";
+//             // string += "<p><strong>Homes with TVs</strong>: " + numberWithCommas(prop["TV Homes"]) + "</p>";
+//             // string += "<p><strong>% with Cable</strong>: " + prop.cableperc + "%</p>";
+//             // string += "<p><strong>Nielsen Rank</strong>: " + prop.Rank + "</p>";
+      
+//             // d3.select("#textbox")
+//             //   .html("")
+//             //   .append("text")
+//             //   .html(string)
+//           .on("mouseout", function(d){
+//             d3.select(this)
+//             .attr("fill", defaultFill)
+//           });
+      
+// }
